e12c3c22343e517f7a4a5bdc0577ce9712220c1b
Daniel Jasso <97632672+Daniel-Jasso-Covarrubias@users.noreply.github.com>
2022-05-16 12:47:33 -0500

    firts commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,7 +1,7 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = 'Hola Jasso';
+const nuevaString = 'Hola Jasso covarrubias';
 
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = null;
